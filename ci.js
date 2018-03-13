569c597f61036bdd5f402e13c83942291188136b
sudeepa14 <sudeepa.14@cse.mrt.ac.lk>
2018-03-13 15:46:38 +0000

    minor

--- ci.js
+++ ci.js
@@ -4,8 +4,9 @@ var shell = require('shelljs');
 exports.builder=function(res){
     shell.echo('this is from shelljs Module');
     shell.cp('-R', 'test.txt/', 'out/test');   
-    shell.exec('sudo npm start --prefix ../youtubeDownloader/react/'); 
     res.sendStatus(200);
+    shell.exec('sudo npm start --prefix ../youtubeDownloader/react/'); 
+    // res.sendStatus(200);
 
 }
     
