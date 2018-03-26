0b6d4ffa82e4f12d583848a08d8f0a786a15dcfc
Sudeepa Nadeeshan <sudeepa.14@cse.mrt.ac.lk>
2018-03-26 14:50:09 +0100

    Update ci.js

--- ci.js
+++ ci.js
@@ -3,8 +3,8 @@ var shell = require('shelljs');
 
 exports.builder=function(res){
     shell.echo('this is from shelljs Module');
-    shell.cp('-R', 'test.txt/', 'out/test');   
-    res.sendStatus(200);
+    //shell.cp('-R', 'test.txt/', 'out/test');   
+    res.sendStatus(200); //responding to github post request
     shell.exec('sudo npm start --prefix ../youtubeDownloader/react/'); 
     // res.sendStatus(200);
 
